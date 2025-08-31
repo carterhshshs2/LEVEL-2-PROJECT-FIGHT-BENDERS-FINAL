414aef5a990305c12698fb2c3071e506ebee082d
carterhshshs2 <stu24206@ashs.school.nz>
2025-08-31 23:34:20 +1200

    FINAL.

--- killzone.gd
+++ killzone.gd
@@ -5,7 +5,9 @@ extends Area2D
 
 func _on_body_entered(body):
 	if Input.is_action_pressed("attack"):
-		print("Worked")
+		print("Parried")
+		self.get_parent().queue_free()
+		
 
 	elif not Input.is_action_pressed("attack"):
 		print("You died!")
