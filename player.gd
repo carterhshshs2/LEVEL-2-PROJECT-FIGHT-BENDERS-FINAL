af139fe9fab01fc71c056c64110f4f8a9f973c08
carterhshshs2 <stu24206@ashs.school.nz>
2025-08-31 22:33:32 +1200

    First product where character is able to move, play and use abilities. Map working, moving platforms working and enemy working.

--- player.gd
+++ player.gd
@@ -3,12 +3,19 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+@onready var playeranimated: AnimatedSprite2D = $Playeranimated
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		$Playeranimated.play("attackground")
+		
+	if Input.is_action_just_pressed("attack") and not is_on_floor():
+		$Playeranimated.play("attackair")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -17,6 +24,21 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		playeranimated.flip_h = false
+	elif direction < 0:
+		playeranimated.flip_h = true
+	
+	if not direction == 0:
+		playeranimated.play("running")
+
+	if not Input.is_anything_pressed():
+		playeranimated.play("default")
+		
+
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
